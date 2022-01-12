4fdcb477a3b20489fbfd1fd609ebdc5b70a061ca
Suzev Atsma Alboris <Suzev.Alboris@atlantica.dev>
2022-01-12 22:42:24 +0700

    Refactor and Rebuild Profile

---
+++ EnrollmentService/SampleRESTAPI/Models/ClassProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace SampleRESTAPI.Models
+{
+    public class ClassProfile : Profile
+    {
+        public ClassProfile()
+        {
+            CreateMap<Models.Course, Dtos.CourseDto>().
+                ForMember(dest => dest.TotalHours,
+                opt => opt.MapFrom(src => src.Credits * 3));
+        }
+    }
+}
