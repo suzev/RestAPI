a59c2414e15e13267566fd09b774591cfa7d044a
Suzev Atsma Alboris <Suzev.Alboris@atlantica.dev>
2022-01-13 11:26:40 +0700

    Create Payment Dto

---
+++ EnrollmentService/PaymentService/Dtos/PaymentInput.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentService
+{
+    public class PaymentInput
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+    }
+}
---
+++ EnrollmentService/PaymentService/Dtos/PaymentOutput.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentService.Dtos
+{
+    public class PaymentOutput
+    {
+        public int PaymentID { get; set; }
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public float Bill { get; set; }
+    }
+}
