01014fe04a0cc482c9cfd9646c0536943c884124
Suzev Atsma Alboris <Suzev.Alboris@atlantica.dev>
2022-01-13 11:22:30 +0700

    add Payment data

---
+++ EnrollmentService/PaymentService/Data/IPayment.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymentService.Data
+{
+    public interface IPayment<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Insert(T obj);
+        Task<T> GetById(int id);
+    }
+}
---
+++ EnrollmentService/PaymentService/Data/PaymentDAL.cs
@@ -0,0 +1,24 @@
+﻿using PaymentService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymentService.Data
+{
+    public class PaymentDAL : IPayment<Payment>
+    {
+        public Task<IEnumerable<Payment>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Payment> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Payment> Insert(Payment obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
