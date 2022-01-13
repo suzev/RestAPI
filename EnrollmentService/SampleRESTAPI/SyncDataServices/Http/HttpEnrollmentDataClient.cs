1b7750e27ddfaef9871dd0a50d37876243b4aee2
Suzev Atsma Alboris <Suzev.Alboris@atlantica.dev>
2022-01-13 20:26:52 +0700

    add syncdata services

---
+++ EnrollmentService/SampleRESTAPI/SyncDataServices/Http/HttpEnrollmentDataClient.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using SampleRESTAPI.Dtos;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SampleRESTAPI.SyncDataServices.Http
+{
+    public class HttpEnrollmentDataClient : IEnrollmentDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpEnrollmentDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task CreateEnrollmentFromPaymentService(CreateEnrollmentDto enrollment)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(enrollment),
+                Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(_configuration["PaymentService"], httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"==> Sync Post to Payment Service Success");
+            }
+            else
+            {
+                Console.WriteLine($"==> Sync Post to Enrollment Service Failed. . .");
+            }
+        }
+    }
+}
---
+++ EnrollmentService/SampleRESTAPI/SyncDataServices/Http/IEnrollmentDataClient.cs
@@ -0,0 +1,10 @@
+﻿using SampleRESTAPI.Dtos;
+using System.Threading.Tasks;
+
+namespace SampleRESTAPI.SyncDataServices.Http
+{
+    public interface IEnrollmentDataClient
+    {
+        Task CreateEnrollmentFromPaymentService(CreateEnrollmentDto enrollment);
+    }
+}
